c8fb248ff4c151772075bfcfd0bb9a94495230b5
Timm Bahmann <timm@bahmann.one>
2019-05-02 22:01:37 +0200

    only serve json requests

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ let express = require('express')
 let router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('Hello World')
+  let contype = req.headers['content-type']
+  if (contype === 'application/json') {
+    res.json({ Hello: 'World' })
+  } else {
+    res.status(415).send()
+  }
 })
 
 module.exports = router
