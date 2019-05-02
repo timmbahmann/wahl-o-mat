aa90796c9b9db50f60553c45727ef94f59306053
Timm Bahmann <timm@bahmann.one>
2019-05-02 22:00:47 +0200

    no view path for now

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 let express = require('express')
-let path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 3001
-
-app.set('views', path.join(__dirname, 'views'))
 
 app.use(require('./routes'))
 
