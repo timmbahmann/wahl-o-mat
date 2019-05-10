8c668b2d4c6bc739e65d250c7dbf2d0bed245833
Timm Bahmann <timm@bahmann.one>
2019-05-10 20:13:15 +0200

    added database connection

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let express = require('express')
 let path = require('path')
+let mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://website:hack-paging8-related-bema9-6talisman-cymbal-madrid8-warn@wahl-o-mat-u1xgc.mongodb.net/test?retryWrites=true', {useNewUrlParser: true})
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -11,4 +14,10 @@ app.use(express.static(path.join(__dirname, 'views')))
 app.listen(PORT, function () {
   console.log(`server started at http://localhost:${PORT}`)
   console.log('Mode:', process.env.NODE_ENV)
+})
+
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  console.log('database connected')
 })
