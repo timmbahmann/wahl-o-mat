effb6fb0dcbf8bbf8abdc004b5bb95adb219752f
Leon Schreiber <leonschreiber96@gmail.com>
2019-05-27 15:28:02 +0200

    Erste Schritte, swing installiert, kleiner Test

--- frontend/js/components/App.vue
+++ frontend/js/components/App.vue
@@ -1,8 +1,11 @@
 <script>
 import Steckbrief from "./Steckbrief";
+import VueSwing from "vue-swing";
+
+// Vue.component('vue-swing', VueSwing)
 
 export default {
-  components: { Steckbrief },
+  components: { Steckbrief, VueSwing },
   data() {
     return {
       pagenum: 0,
@@ -10,7 +13,10 @@ export default {
       wahlname: "",
       gremium: "",
       firstName: "",
-      age: 0
+      age: 0,
+      config: {
+        // throwOutConfidence: 0.5
+      }
     };
   },
   computed: {
@@ -31,12 +37,18 @@ export default {
   methods: {
     fetchAge() {
       this.age = Math.random() * 100;
+    },
+    throwout() {
+      console.log("Just threw a card out :)");
+    },
+    throwin() {
+      console.log("Just threw one in!");
     }
   }
 };
 </script>
 <template>
-  <div>
+  <div style="display:flex">
     <h1>Wahl-o-Mat</h1>
     <h2>{{ wahlname }}</h2>
     <h3>{{ gremium }}</h3>
@@ -46,5 +58,26 @@ export default {
     <input type="text" v-model="firstName">
     <button @click="fetchAge">fetch</button>
     <Steckbrief :firstName="firstName" lastName="Obermeier" :age="age"/>
+    <vue-swing @throwout="throwout" @throwin="throwin" :config="config">
+      <div v-for="these in thesen" class="box">
+        <div>These:</div>
+        <div>{{ these.these }}</div>
+        <div style="display:flex;"> 
+          <div v-for="antwort in these.antworten">
+            <div style="font-weight:bold;margin:10px;">{{ antwort.name }}</div>
+            <div>{{ antwort.antwort }}</div>
+          </div>
+        </div>
+      </div>
+    </vue-swing>
   </div>
 </template>
+<style scoped>
+.box {
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  background: white;
+  border-radius: 3px;
+  padding: 1.2em;
+  width: 80%;
+}
+</style>
