924402ec29dc6707991f652a4aab4db046dc7239
Leon Schreiber <leonschreiber96@gmail.com>
2019-05-29 21:07:32 +0200

    Removed backend stuff that doesnt work but was still merged

--- routes/deletewahl.route.js
+++
@@ -1,9 +0,0 @@
-let wahlcontroller = require('../controller/wahl.controller')
-
-module.exports = async function (req, res, next) {
-  let wahlname = decodeURI(req.params.wahlname)
-  wahlcontroller
-    .deleteWahl(wahlname)
-    .then(() => res.json({ ok: 1 }))
-    .catch(next)
-}
--- routes/index.route.js
+++ routes/index.route.js
@@ -5,17 +5,10 @@
 let express = require('express')
 let wahlmiddleware = require('../middlewares/wahl.validate')
 let langmiddleware = require('../middlewares/getlang.middleware')
-let createwahlroute = require('./createwahl.route')
+
 let getjsonroute = require('./getjson.route')
 let getwahlroute = require('./getwahl.route').getwahl
 let getEveryWahlroute = require('./getwahl.route').getEveryWahl
-let updatewahlroute = require('./updatewahl.route')
-let deletewahlroute = require('./deletewahl.route')
-let createuserroute = require('./createuser.route')
-let getuserroute = require('./getuser.route').getuser
-let getEveryUserroute = require('./getuser.route').getEveryUser
-let updateuserroute = require('./updateuser.route')
-let deleteuserroute = require('./deleteuser.route')
 
 let router = express.Router()
 
@@ -25,13 +18,6 @@ let router = express.Router()
  */
 
 router.get('api/json', langmiddleware, getjsonroute)
-
-/**
- * Gets a wahl request, validates it inside the middleware and
- * saves the data in the DB
- */
-
-router.post('api/wahl/:wahlname', langmiddleware, wahlmiddleware, createwahlroute)
 
 /**
  * Get a wahl request and returns the corresponding wahl object from the database
@@ -44,47 +30,5 @@ router.get('api/wahl/:wahlname', langmiddleware, getwahlroute)
  */
 
 router.get('api/wahl', langmiddleware, getEveryWahlroute)
-
-/**
- * Update a wahl
- */
-
-router.put('api/wahl/:wahlname', langmiddleware, updatewahlroute)
-
-/**
- * Delete a wahl
- */
-
-router.delete('api/wahl/:wahlname', langmiddleware, deletewahlroute)
-
-/**
- * create a user
- */
-
-router.post('api/user/:username', langmiddleware, createuserroute)
-
-/**
- * get a user
- */
-
-router.get('api/user/:username', langmiddleware, getuserroute)
-
-/**
- * get all users
- */
-
-router.get('api/user', langmiddleware, getEveryUserroute)
-
-/**
- * Update a user
- */
-
-router.put('api/user/:username', langmiddleware, updateuserroute)
-
-/**
- * delete a user
- */
-
-router.delete('api/user/:username', langmiddleware, deleteuserroute)
 
 module.exports = router
