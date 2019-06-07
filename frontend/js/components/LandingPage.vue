d7ffc2297cf99c40ed04d092860aa4ea155001ba
michellekappl <michellekappl@campus.tu-berlin.de>
2019-06-07 14:19:21 +0200

    Startseite

--- frontend/js/components/LandingPage.vue
+++ frontend/js/components/LandingPage.vue
@@ -1,13 +1,71 @@
 <script>
+import landingpageComponent from "./landingpageComponent";
+import Wahlomat from "./Wahlomat";
+
 export default {
-  components: {},
+  components: { landingpageComponent, Wahlomat },
   data() {
-    return {};
+    return {
+      displayedComponent: "landingPage",
+      election: {},
+
+    }
+  },
+  methods: {
+      switchPage(page) {
+      this.displayedComponent = page;
+    },
   }
 };
+
 </script>
 <template>
-  <div>This is the landing page</div>
+ 
+  <div class = "bodytext content">
+   <h1>Startseite</h1>
+      <p class ="text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+         sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
+         aliquyam erat, sed diam voluptua. At vero eos et accusam et justo 
+         duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
+         sanctus est Lorem ipsum dolor sit amet.
+      </p>
+
+
+    <landingpageComponent gremium = "fakrat"  name="wahl 2019" @start="switchPage('Wahlomat')" />
+     <p>
+       <a href="http://lmgtfy.com/?q=Wo+kann+ich+w%C3%A4hlen%3F">Wo kann ich wählen?</a> 
+     </p>
+     <p>
+       <a href="http://lmgtfy.com/?q=Wann+kann+ich+w%C3%A4hlen%3F">Wann kann ich wählen?</a>
+     </p>
+  </div>
+  
+     
 </template>
 <style scoped>
+
+  .bodytext {
+    font-family: Helvetica;
+    color: white;
+  }
+  .text{
+    
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  a:link {
+    color: white;
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  a:visited {
+    color: white;
+  }
+
 </style>
+
---
+++ frontend/js/components/landingpageComponent.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+name: "landingpageComponent",
+props: ["gremium", "name"],
+};
+</script>
+
+<template>
+ <div class = "box">
+    <div class ="content">
+
+     <div class = "wahl"> {{ gremium }} </div>
+     <div class = "wahl"> {{ name }} </div>
+     <button class= "button" @click="$emit('start')"> Starten </button>
+
+    </div>
+ </div> 
+</template>
+
+<style scoped>
+.box {
+    background-color: #fff;
+    width: 80%;
+    /* max-width: 80%; */
+    /* max-height: 80%; */
+    border-radius: 10px;
+    color: #333;
+}
+
+.content{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 5px;
+}
+.wahl {
+    margin-bottom: 10px;
+
+}
+.button {
+    background: linear-gradient(135deg, #eae2d2 0, #d9ceb0);
+    color: #333;
+    border-radius: 4px;
+    cursor: pointer;
+    border: none;
+    padding: 2px 5px 2px 5px;
+}
+
+</style> 
